73a28ec5825323f0db23906c839a7209055ceb45
Roberto Borges <borges.tj@gmail.com>
2020-07-17 21:24:30 -0300

    Adjusted color pokemon name

--- src/components/PokeList.vue
+++ src/components/PokeList.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="container">
-      <p>{{ items.name }}</p>
-        <div class="cards">
-          <div class="card" v-for="pokemons in items.pokemon_species" :key="pokemons.id">
-          <img class="image" :src="`https://img.pokemondb.net/artwork/${pokemons.name}.jpg`" :alt="pokemons.name">
-          <h3>{{ pokemons.name }}</h3>
-        </div>
+  <div class="container">
+    <p>{{ items.name }}</p>
+    <div class="cards">
+      <div class="card" v-for="pokemons in items.pokemon_species" :key="pokemons.id">
+        <img class="image" :src="`https://img.pokemondb.net/artwork/${pokemons.name}.jpg`" :alt="pokemons.name">
+        <h3>{{ pokemons.name }}</h3>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -28,19 +28,23 @@ export default {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
 
-  .card {
-    box-shadow: 0px 0px 6px 1px rgba(0, 0, 0, 0.2);
-    padding: 10px;
-    margin: 10px;
-    width: 240px;
-  }
+    .card {
+      box-shadow: 0px 0px 6px 1px rgba(0, 0, 0, 0.2);
+      padding: 10px;
+      margin: 10px;
+      width: 240px;
 
-  .image {
-    padding: 10px;
-    width: 220px;
-    height: 249px;
+      .image {
+        padding: 10px;
+        width: 220px;
+        height: 249px;
+      }
+
+      h3 {
+        color: #151515;
+      }
+    }
   }
-}
 }
 
 @media (max-width: 1025px) {
